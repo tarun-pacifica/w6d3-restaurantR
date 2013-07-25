89682512497367d1ed00feb62c7c61832688a4e0
PACIFICA <MOOKBOOK13@Taruns-MacBook-Air.local>
2013-07-25 10:43:34 +1000

    work presented to class

--- app/assets/javascripts/tmap.js
+++ app/assets/javascripts/tmap.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
   var styles = [
     {
       stylers: [
-        { hue: "#00ffe6" },
-        { saturation: -20 }
+        { hue: "#890000" },
+        { saturation: -20 },
+        { visibility: 'simplified' },
+        { gamma: 0.5 },
+        { weight: 0.5 }
       ]
     },{
       featureType: "road",
@@ -23,13 +26,26 @@ $(document).ready(function() {
       featureType: "road",
       elementType: "geometry.fill",
       stylers: [
-        { invert_lightness: true },
+        { invert_lightness: false },
         { gamma: 1 },
         { visibility: "simplified" },
         { weight: 1.2 }
       ]
+    },{
+      featureType: 'water',
+      stylers: [
+        { color: '#890000' }
+      ]
     }
   ]
   map.setOptions({styles: styles});
+
+  // marker = new google.maps.Marker({
+  //   map:map,
+  //   draggable:true,
+  //   animation: google.maps.Animation.DROP,
+  //   position: center
+  // });
+  // google.maps.event.addListener(marker, 'click', toggleBounce);
 
 });
